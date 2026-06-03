8c8ed61a9af2b9b37885de1103967c32431101bd
Artem Belyaev <belyaevartemg@gmail.com>
2026-06-04 01:28:04 +0300

    Исправлена ошибка при которой не создавался список ингридиентов для рецептов

--- src/DietaryRecipe.py
+++ src/DietaryRecipe.py
@@ -2,7 +2,7 @@ from Recipe import *
 from Ingredient import *
 
 class DietaryRecipe(Recipe):
-    def __init__(self, title, diet_type, ingredients = None):
+    def __init__(self, title, diet_type, ingredients = []):
         super().__init__(title, ingredients)
         self.diet_type = diet_type
 
--- src/Recipe.py
+++ src/Recipe.py
@@ -1,7 +1,7 @@
 from Ingredient import *
 
 class Recipe:
-    def __init__(self, title, ingredients = None):
+    def __init__(self, title, ingredients = []):
         self.title = title
         self.ingredients = ingredients
 
