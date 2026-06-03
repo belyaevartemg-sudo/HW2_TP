6c52356892f37686dccf0c7679c0b8545fca2a41
Artem Belyaev <belyaevartemg@gmail.com>
2026-06-03 16:22:31 +0300

    Добавлен класс DietaryRecipe.py со всеми нужными методами на данный момент по заданию

--- src/DietaryRecipe.py
+++ src/DietaryRecipe.py
@@ -1,4 +1,15 @@
+from Recipe import *
+from Ingredient import *
+
 class DietaryRecipe(Recipe):
     def __init__(self, title, diet_type, ingredients = None):
-        self.name = title
-        self.ingredients = ingredients
+        super().__init__(title, ingredients)
+        self.diet_type = diet_type
+
+    def scale(self, ratio: float):
+        new_recipe = super().scale(ratio)
+        return DietaryRecipe(new_recipe.title, self.diet_type, new_recipe.ingredients)
+
+    def __str__(self):
+        text = "[" + self.diet_type + "] " + super().__str__()
+        return text
