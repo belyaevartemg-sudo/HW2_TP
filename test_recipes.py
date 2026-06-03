2fea34b894d2b10cf2b2e33f7e128b2931a66f50
Artem Belyaev <belyaevartemg@gmail.com>
2026-06-04 02:07:25 +0300

    Добавлены все тесты для ShoppingList

--- test_recipes.py
+++ test_recipes.py
@@ -26,13 +26,13 @@ def test_recipe():
 def test_recipe_add_ingredient_1():
     x = Recipe("Хлеб")
     x.add_ingredient(Ingredient("Мука", 500, "г"))
-    assert x.ingredients == [Ingredient("Мука", 500, "г")]
+    assert x.ingredients == [Ingredient("Мука", 500, "г")] and x.ingredients[0].quantity == 500
 
 def test_recipe_add_ingredient_2():
     x = Recipe("Хлеб")
     x.add_ingredient(Ingredient("Мука", 500, "г"))
     x.add_ingredient(Ingredient("Мука", 500, "г"))
-    assert x.ingredients == [Ingredient("Мука", 1000, "г")]
+    assert x.ingredients == [Ingredient("Мука", 1000, "г")] and x.ingredients[0].quantity == 1000
 
 def test_recipe_scale_1():
     x = Recipe("Хлеб")
@@ -43,9 +43,87 @@ def test_recipe_scale_2():
     x = Recipe("Хлеб")
     x.add_ingredient(Ingredient("Мука", 500, "г"))
     x.add_ingredient(Ingredient("Вода", 1000, "г"))
-    assert x.scale(2).ingredients == [Ingredient("Мука", 1000, "г"), Ingredient("Вода", 2000, "г")]
+    assert (x.scale(2).ingredients == [Ingredient("Мука", 1000, "г"), Ingredient("Вода", 2000, "г")]
+            and x.scale(2).ingredients[0].quantity == 1000) and x.scale(2).ingredients[1].quantity == 2000
 
 def test_scale_3():
     x = Recipe("Хлеб")
     with pytest.raises(ValueError):
         x.scale(-1)
+
+def test_shopping_list_add_recipe_1():
+    x = Recipe("Хлеб")
+    x.add_ingredient(Ingredient("Мука", 500, "г"))
+    sl = ShoppingList()
+    sl.add_recipe(x, 2)
+    assert sl.get_list() == [Ingredient("Мука", 1000, "г")] and sl.get_list()[0].quantity == 1000
+
+def test_shopping_list_add_recipe_2():
+    sl = ShoppingList()
+    x = Recipe("Хлеб")
+    x.add_ingredient(Ingredient("Мука", 500, "г"))
+    with pytest.raises(ValueError):
+        sl.add_recipe(x, 0)
+
+def test_shopping_list_remove_recipe_1():
+    x = Recipe("Хлеб")
+    x.add_ingredient(Ingredient("Мука", 500, "г"))
+    x.add_ingredient(Ingredient("Вода", 1000, "г"))
+    sl = ShoppingList()
+    sl.add_recipe(x, 2)
+    sl.remove_recipe("Хлеб")
+    assert sl.get_list() == []
+
+def test_shopping_list_remove_recipe_2():
+    x = Recipe("Хлеб")
+    x.add_ingredient(Ingredient("Мука", 500, "г"))
+    sl = ShoppingList()
+    sl.add_recipe(x, 2)
+    sl.remove_recipe("Пиво")
+    assert sl.get_list() == [Ingredient("Мука", 1000, "г")] and sl.get_list()[0].quantity == 1000
+
+def test_shopping_list_get_list_1():
+    x1 = Recipe("Хлеб")
+    x1.add_ingredient(Ingredient("Мука", 500, "г"))
+    x2 = Recipe("Пиво")
+    x2.add_ingredient(Ingredient("Мука", 1000, "г"))
+    sl = ShoppingList()
+    sl.add_recipe(x1, 1)
+    sl.add_recipe(x2, 1)
+    sl_list = sl.get_list()
+    assert len(sl_list) == 1 and sl_list[0].quantity == 1500
+
+def test_shopping_list_get_list_2():
+    x1 = Recipe("Хлеб")
+    x1.add_ingredient(Ingredient("Мука", 500, "г"))
+    x2 = Recipe("Пиво")
+    x2.add_ingredient(Ingredient("Вода", 1000, "г"))
+    sl = ShoppingList()
+    sl.add_recipe(x1, 1)
+    sl.add_recipe(x2, 1)
+    sl_list = sl.get_list()
+    assert sl_list[0].name == "Вода" and sl_list[1].name == "Мука"
+
+def test_shopping_list_add_1():
+    x1 = Recipe("Хлеб")
+    x1.add_ingredient(Ingredient("Мука", 500, "г"))
+    x2 = Recipe("Пиво")
+    x2.add_ingredient(Ingredient("Вода", 1000, "г"))
+    sl1 = ShoppingList()
+    sl2 = ShoppingList()
+    sl1.add_recipe(x1, 1)
+    sl2.add_recipe(x2, 1)
+    sl_list = (sl1 + sl2).get_list()
+    assert sl_list[0].name == "Вода" and sl_list[1].name == "Мука"
+
+def test_shopping_list_add_2():
+    x1 = Recipe("Хлеб")
+    x1.add_ingredient(Ingredient("Мука", 500, "г"))
+    x2 = Recipe("Пиво")
+    x2.add_ingredient(Ingredient("Вода", 1000, "г"))
+    sl1 = ShoppingList()
+    sl2 = ShoppingList()
+    sl1.add_recipe(x1, 1)
+    sl2.add_recipe(x2, 1)
+    sl3 = sl1 + sl2
+    assert len(sl1.get_list()) == 1 and len(sl2.get_list()) == 1 and len(sl3.get_list()) == 2
