20567039fc4783d036e3dfb2666359c2470cd1ee
Artem Belyaev <belyaevartemg@gmail.com>
2026-06-04 01:29:15 +0300

    Добавлены файлы с тестами на pytest и отдельно настройки pytest. Написаны тесты для Ingredient

---
+++ test_recipes.py
@@ -0,0 +1,30 @@
+import pytest
+from Ingredient import *
+from Recipe import *
+from ShoppingList import *
+
+def test_ingredient():
+    x = Ingredient("Мука", 500, "г")
+    assert x.name == "Мука" and x.quantity == 500.0 and x.unit == "г"
+
+def test_ingredient_str():
+    assert str(Ingredient("Мука", 500, "г")) == "Мука: 500.0 г"
+
+def test_ingredient_eq_1():
+    assert Ingredient("Мука", 500, "г") == Ingredient("Мука", 100, "г")
+
+def test_ingredient_eq_2():
+    assert Ingredient("Мука", 500, "г") != Ingredient("Лук", 500, "г")
+
+def test_ingredient_eq_3():
+    assert Ingredient("Мука", 500, "г") != Ingredient("Мука", 500, "кг")
+
+def test_recipe():
+    x = Recipe("Хлеб")
+    assert x.title == "Хлеб" and x.ingredients == []
+
+def test_recipe_add_ingredient_1():
+    x = Recipe("Хлеб")
+    x.add_ingredient(Ingredient("Мука", 500, "г"))
+    assert x.ingredients == [Ingredient("Мука", 500, "г")]
+
